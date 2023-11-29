2b5d4b91339935ff59932f7e49704811a7a2a1d1
JulieMontoux <julie.montoux@epsi.fr>
2023-11-29 19:34:58 +0100

    ajout du choix de la langue

--- pokedex/src/Pokedex.js
+++ pokedex/src/Pokedex.js
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-const Pokemon = ({ pokemon, typesData }) => {
+const Pokemon = ({ pokemon, typesData, language }) => {
   const cardStyle = {
     flexWrap: 'wrap',
     backgroundColor: '#FFA10A',
@@ -37,14 +37,14 @@ const Pokemon = ({ pokemon, typesData }) => {
 
   const getTypeName = (typeId) => {
     const type = typesData.find(type => type.id === typeId);
-    return type ? type.name.fr : 'Inconnu';
+    return type ? type.name[language] : 'Inconnu';
   };
 
   return (
     <div style={cardStyle}>
       <img src={pokemon.image} alt={`${pokemon.name.en} sprite`} style={imageStyle} />
       <div style={contentStyle}>
-        <h2 style={h2Style}>{pokemon.name.fr}</h2>
+        <h2 style={h2Style}>{pokemon.name[language]}</h2>
         <p style={pStyle}>ID: {pokemon.id}</p>
         <p style={pStyle}>
           Type(s): {pokemon.types.map((typeId, index) => (
@@ -59,6 +59,7 @@ const Pokemon = ({ pokemon, typesData }) => {
 const App = () => {
   const [pokemonList, setPokemonList] = useState([]);
   const [typesData, setTypesData] = useState([]);
+  const [language, setLanguage] = useState('en'); // Par défaut en anglais
   const [error, setError] = useState(null);
 
   useEffect(() => {
@@ -83,6 +84,10 @@ const App = () => {
     fetchData();
   }, []);
 
+  const handleLanguageChange = (newLanguage) => {
+    setLanguage(newLanguage);
+  };
+
   if (error) {
     return <p>Error loading data. Please check the console for details.</p>;
   }
@@ -93,10 +98,15 @@ const App = () => {
 
   return (
     <>
-      <h1 style={{ textAlign: 'center', color: '#e53935', fontSize: 'xx-large', fontWeight: '900' }}>Pokédex</h1>
+      <div style={{ marginTop: '20px', textAlign: 'right', marginRight: '100px'}}>
+        <img src="https://cdn-icons-png.flaticon.com/128/8363/8363075.png" alt="English flag" onClick={() => handleLanguageChange('en')} style={{ marginRight: '10px', width: '30px' }}/>
+        <img src="https://cdn-icons-png.flaticon.com/128/5921/5921991.png" alt="French flag" onClick={() => handleLanguageChange('fr')} style={{ marginRight: '10px', width: '30px' }}/>
+      </div>
+      <h1 style={{ textAlign: 'center', color: '#e53935', marginTop: '-20px'}}>Pokédex - Julie Montoux</h1>
+      <p style={{ textAlign: 'center', color: '#e53935', marginTop: '-20px', textDecoration: 'underline'}}>Projet React</p>
       <div style={{ maxWidth: '1200px', margin: '0 auto', padding: '20px', display: 'grid', gridTemplateColumns: 'repeat(auto-fill, minmax(300px, 1fr))', gap: '20px' }}>
         {pokemonList.map(pokemon => (
-          <Pokemon key={pokemon.id} pokemon={pokemon} typesData={typesData} />
+          <Pokemon key={pokemon.id} pokemon={pokemon} typesData={typesData} language={language} />
         ))}
       </div>
     </>
